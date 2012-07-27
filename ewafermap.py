7b643fa81ba22e7f673d38619cb64295ac69ea1d
Brecht Hoflack <brh@melexis.com>
2012-07-27 14:45:14 +0200

    Initial version

---
+++ ewafermap.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+
+from base64 import b64decode
+from jinja2 import Template
+from xml.sax import handler, parseString
+
+class Lot:
+
+    def __init__(self, name, item, wafersInLot, organization, probelocation, subcontractor, wafers=[], config={}):
+        self.name = name
+        self.item = item
+        self.wafersInLot = wafersInLot
+        self.organization = organization
+        self.probelocation = probelocation
+        self.subcontractor = subcontractor
+        self.wafers = wafers
+        self.config = config
+
+    def __repr__(self):
+        return "Lot {name: %s, item: %s, wafersInLot: %s, organization: %s, probelocation: %s, subcontractor: %s, wafers: %s, config: %s}" % (self.name, self.item, self.wafersInLot, self.organization, self.probelocation, self.subcontractor, self.wafers, self.config)
+
+class Wafer:
+
+    def __init__(self, number, passdies, wafermaps = [], config={}):
+        self.number = number
+        self.passdies = passdies
+        self.wafermaps = wafermaps
+        self.config = config
+
+    def __repr__(self):
+        return "Wafer {number: %s, passdies: %s, wafermaps: %s, config: %s}" % (self.number, self.passdies, self.wafermaps, self.config)
+
+class Wafermap:
+
+    def __init__(self, name, formats={}):
+        self.name = name
+        self.formats = formats
+
+    def __repr__(self):
+        return "Wafermap {name: %s, formats: %s}" % (self.name, self.formats)
+
+class Format:
+
+    def __init__(self, encoding, wafermap):
+        self.encoding = encoding
+        self.wafermap = wafermap
+
+    def decode(self):
+        if self.encoding == 'base64':
+          return b64decode(self.wafermap)
+        else:
+          raise "Invalid type"
+
+    def __repr__(self):
+        return "Format {encoding: %s, wafermap: %s}" % (self.encoding, self.wafermap)
+
+class LotHandler(handler.ContentHandler):
+
+    def __init__(self):
+        self.inLot = False
+        self.inWafer = False
+        self.inWaferProperties = False
+        self.inWafermaps = False
+        self.inWafermap = False
+        self.inFormats = False
+        self.inFormat = False
+
+    def startElement(self, name, attrs):
+        if name == 'lot':
+            self.inLot = True
+            self.la = dict(attrs)
+            self.wafers = []
+        elif name == 'wafer':
+            self.inWafer = True
+            self.wa = dict(attrs)
+            self.wafermaps = []
+        elif name == 'wafer-properties':
+            self.inWaferProperties = True
+            self.waferProperties = {}
+        elif self.inWaferProperties and name == 'parameter':
+            self.waferProperties[attrs.get('key')] = attrs.get('value')
+        elif name == 'wafermaps':
+            self.inWafermaps = True
+            self.wafermaps = {}
+        elif name == 'wafermap':
+            self.inWafermap = True
+            self.wafermapName = attrs.get('name')
+        elif name == 'formats':
+            self.inFormats = True
+            self.formats = {}
+        elif name == 'format':
+            self.inFormat = True
+            self.formatName = attrs.get('name')
+            self.formatEncoding = attrs.get('encoding')
+            self.formatContent = ""
+
+    def endElement(self, name):
+        if name == 'lot':
+            self.inLot = False
+            self.lot = Lot(self.la.get("name"), self.la.get('item'), self.la.get('wafersInLot'), self.la.get('organization'), self.la.get('probelocation'), self.la.get('subcontractor'), self.wafers)
+        elif name == 'wafer':
+            self.inWafer = False
+            self.wafers.append(Wafer(self.wa.get("number"), self.wa.get("passdies"), self.wafermaps, self.waferProperties))
+        elif name == 'wafer-properties':
+            self.inWaferProperties = False
+        elif name == 'wafermaps':
+            self.inWafermaps = False
+        elif name == 'wafermap':
+            self.inWafermap = False
+            self.wafermaps[self.wafermapName] = Wafermap(self.wafermapName, self.formats)
+        elif name == 'formats':
+            self.inFormats = False
+        elif name == 'format':
+            self.formats[self.formatName] = Format(self.formatEncoding, self.formatContent)
+            self.inFormat = False
+            
+    def characters(self, content):
+        if self.inFormat == True:
+            self.formatContent = self.formatContent + content.strip()
+
+
+def decode(body):
+  """Parse an xml body to objects
+
+     This function will convert an xml message according to the lot.xsd schema to a lot object.
+
+     As an xml message can be big,  we load it from file.
+     >>> msg = open('example.xml').read()
+
+     Now call parse with the xml message,  this should return a parsed lot object.
+     >>> l = decode(msg)
+     >>> l.name
+     u'M31265'
+     >>> l.wafersInLot
+     u'4'
+     >>> l.wafers[0].config
+     {u'buildAt': u'20120302T11:53', u'origin': u'MapMerge', u'site': u'erfurt', u'processStep': u'pactech'}
+  """
+  handler = LotHandler()
+  parseString(body, handler)
+  return handler.lot
+
+def encode(lot):
+  """Encode a lot to xml.
+
+     First create the lot you want to encode
+     >>> w1 = Wafer(1, 100, [Wafermap("blaat", {"TH01": Format("base64", "aGVsbG8gd29ybGQ=")})])
+     >>> w2 = Wafer(2, 200, [Wafermap("blubber", {"TH01": Format("base64", "a3J1c3R5")})])
+     >>> l = Lot("A12345", "201210600", 2, "IEPER", "IEPER", "MLX_BOGUS", [w1, w2], {'val1': 'blub'})
+  
+     Now encode the lot to xml    
+     >>> s = encode(l)
+
+     Read the expected format
+     >>> with open('expected_lot.xml', 'r') as f:
+     ...   expected = f.read()
+     ...   s == expected
+     True
+  """
+  t = open('lot.xml').read()
+  template = Template(t)
+  return template.render(lot=lot)
+
+def eachWafer(l, f):
+  """Iterate each wafer in a lot and call f on the wafer
+
+     F is a function that takes one parameter ( Wafer )
+
+     Example:
+     
+     A lot with two wafers
+     >>> l = Lot('A12345', '201210600', 2, 'IEPER', 'IEPER', 'MLX_BOGUS', [Wafer(1, 100), Wafer(2, 200)])
+
+     When we call eachWafer with the lot the function should be executed on both wafers.
+     Create a real function as lambda is limited.
+     >>> def printNumber(w): print w.number
+     >>> eachWafer(l, printNumber)
+     1
+     2
+  """
+  for w in l.wafers:
+      f(w)
+
+def eachWafermap(l, f):
+  """Iterate each wafermap in a lot and call f on the wafermap
+
+     F is a function that takes two parameters ( Wafer, Wafermap )
+
+     Example:
+     
+     A lot with two wafers
+
+     >>> w1 = Wafer(1, 100, [Wafermap('test'), Wafermap('test2')])
+     >>> w2 = Wafer(2, 200, [Wafermap('blubber')])
+     >>> l = Lot('A12345', '201210600', 2, 'IEPER', 'IEPER', 'MLX_BOGUS', [w1, w2])
+     
+     When we call eachWafermap with the lot the function should be executed on every wafermap.
+     >>> def printWafermapName(_, wm): print wm.name
+     >>> eachWafermap(l, printWafermapName)
+     test
+     test2
+     blubber
+  """
+  def _iterateWafermap(w): 
+    for wm in w.wafermaps:
+      f(w, wm)
+  eachWafer(l, _iterateWafermap)
+
+def _test():
+  import doctest
+  doctest.testmod()
+
+if __name__ == '__main__':
+  _test()
---
+++ mapmerge.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+import base64
+import os
+import subprocess
+import stomp
+import sys
+
+from ewafermap import *
+from tempfile import mkdtemp
+
+MAPMERGE = '/usr/share/ink-tool/bin/inkless'
+
+def save_wafermap_formats_to_dir(wafermaps, d):
+  """Save a wafermap in the given directory.
+
+     Wafermaps is a tuple containing the name of the wafermap and the base64 encoded wafermap.
+  """
+  for (name, wafermap) in wafermaps:
+    with open(d + '/' + name, 'wb') as f:
+      decoded = base64.b64decode(wf.wafermap)
+      f.write(decoded)
+
+def th01_wafermaps_generator(wafer):
+  """Generator that selects all th01 wafermaps from a given wafer"""
+  for wm in wafer.wafermaps:
+    fm = wm.formats['TH01']
+    yield (wm.name, fm.decode())
+    
+def mapmerge(wafer):
+  """Call mapmerge for a given wafermap.  Save the result of mapmerge in a new wafermap."""
+  # create the temporary directoy for the input wafermaps
+  ind = mkdtemp(suffix='input')
+  outd = mkdtemp(suffix='output')
+
+    
+  # select all th0x wafermaps to save in the in directory
+  wafermaps = th01_wafermaps_generator(wafer)
+
+  # save all selected wafermaps in the in directory
+  save_wafermap_formats_to_dir(wafermaps, ind)
+
+  try:
+    # spawn a new subprocess for mapmerge
+    child = subprocess.Popen([
+        MAPMERGE, 
+        "wafer=%d" % wafer.number, 
+        "ProcessStep=%s" % wafer.config['ProcessStep'],
+        "noDB",
+        "localFolder=%s" % ind,
+        "DestinationDir=%s" % outd
+        ], 
+                             stdout=subprocess.PIPE, 
+                             stderr=subprocess.PIPE)
+
+    # block until the command is completed
+    child.wait()
+
+    # trigger an exception when the returncode isn't 0
+    if child.returncode != 0:
+        stdout = child.stdout.read()
+        stderr = child.stderr.read()
+        raise """Got return code different then 0:  %d
+                 stdout: %s
+                 stderr: %s""" % (child.returncode, stdout, stderr)
+  finally:
+    child.stdout.close()
+    child.stderr.close()
+
+  # check for generated wafermaps in the out directory
+  files = [ind + '/' + f for f in os.listdir(ind)]
+
+  for filename in files:
+    f = open(filename, 'rb').read()
+    wafermap = Wafermap(filename, {'TH01': Format('base64', base64.b64encode(f))})
+    wafer.wafermaps.append(wafermap)
+
+class MessageListener:
+
+  def __init__(self, conn):
+    self.conn = conn
+
+  def on_error(self, headers, message):
+    print 'Got error %s' % message
+
+  def on_message(self, headers, message):
+    lot = decode(message)
+    
+    # perform mapmerge on each wafer
+    eachWafer(lot, mapmerge)
+    
+    response = encode(lot)
+    # send the result back
+    self.conn.send(response, destination='/queue/response')
+    
+    
+def listen(hostname, port):
+  while True:
+    conn = stomp.Connection([(hostname, port)])
+    conn.set_listener('', MessageListener(conn))
+    conn.start()
+    conn.connect()
+
+    conn.subscribe(destination='/queue/request', ack='auto')
+
+    conn.disconnect()
+
+def usage():
+  print("Usage:  %s <<hostname>> <<port>>" % sys.argv[0])
+
+def main():
+  if len(sys.argv) != 3:
+    usage()
+  else:
+    [program, hostname, port] = sys.argv
+    listen(hostname, int(port))
+
+if __name__ == '__main__':
+  main()
