c5dc9b1fd6a056ab807333d6f08ddc2ea32739e0
Brecht Hoflack <brh@melexis.com>
2012-09-05 14:07:00 +0200

    Connect to the correct stomp port

--- stomptest.py
+++ stomptest.py
@@ -5,14 +5,14 @@ class Listener:
   def on_message(self, headers, message):
     print "got %s" % message
 
-conn = stomp.Connection([("ewaf-test.colo.elex.be", 61616)])
+conn = stomp.Connection([("ewaf-test.colo.elex.be", 61613)])
 conn.set_listener('', Listener())
 conn.start()
 conn.connect()
 
 conn.subscribe(destination='/queue/test', ack='auto')
 
-conn.send('test', destination='/queue/test')
+conn.send('ping', destination='/queue/test')
 
 import time
 time.sleep(100)
