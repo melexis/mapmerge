1615a204e5e65c93efa2276d7de07e1912c7bf98
Brecht Hoflack <brh@melexis.com>
2012-09-28 09:00:33 +0200

    Replace stompy with stomp

--- stompy/__init__.py
+++
@@ -1,4 +0,0 @@
-from stompy.stomp import Stomp, NotConnectedError
-from stompy.simple import Client, Empty, TransactionError
-from stompy.distmeta import __version__
-from stompy.distmeta import __doc__, __author__, __contact__, __homepage__
