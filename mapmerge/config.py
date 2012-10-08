45d0b389429d15950aab525ded9470541314796d
Lien Deboosere <leb@melexis.com>
2012-10-08 11:58:55 +0200

    INKLESS-1046: add full path to log files

--- mapmerge/config.py
+++ mapmerge/config.py
@@ -8,6 +8,6 @@ STOMP_PORT=61613
 
 WMDS_WEBSERVICE = 'http://ewaf-test.colo.elex.be:8181/cxf/api/wafermap/'
 
-LOGFILE = 'log/mapmerge.log'
+LOGFILE = '/usr/share/python-support/python-mapmerge/mapmerge/log/mapmerge.log'
 LOGLEVEL = logging.DEBUG
 LOGHANDLER = logging.handlers.RotatingFileHandler(LOGFILE, maxBytes=524288, backupCount=10)
