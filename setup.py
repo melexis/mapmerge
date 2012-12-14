b65fe151a940cd621ed97d16e035b0de66931902
Jerome Morel <jem@melexis.com>
2012-12-14 11:52:57 +0100

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='mapmerge',
     description='electronic wafermap processor for mapmerge',
-    version='1.0.0',
+    version='1.0.1',
     long_description=__doc__,
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", '*send_job.py', '*stomptest.py']),
     zip_safe=False,
